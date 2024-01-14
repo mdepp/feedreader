777e572ee82ddb115e211dad2d94a6c0c5439e30
rain <31737634+mdepp@users.noreply.github.com>
2024-01-14 11:18:19 -0700

    Improve sync logging

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -2,7 +2,9 @@ import database from "~/services/database.server";
 
 async function main() {
   while (true) {
+    console.log("Syncing feeds");
     await database.sync();
+    console.log("Finished sync, waiting an hour");
     await new Promise((resolve) => setTimeout(resolve, 1000 * 60 * 60));
   }
 }
