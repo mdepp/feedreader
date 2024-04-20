2f2d124f9855a589f1c57ca9c12a7938e33f5467
rain <31737634+mdepp@users.noreply.github.com>
2024-04-20 16:52:22 -0600

    Add same-site and secure properties to user-prefs

--- app/cookies.server.ts
+++ app/cookies.server.ts
@@ -2,4 +2,6 @@ import { createCookie } from "@remix-run/node";
 
 export const userPrefs = createCookie("user-prefs", {
   maxAge: 604_800, // one week
+  sameSite: "lax",
+  secure: process.env.NODE_ENV === "production",
 });
